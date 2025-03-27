02cb796126d02d87ceb04f198cdf423f7cdead6e
BcDemir <burakcdemir@gmail.com>
2025-03-27 15:31:40 -0400

    docs: updated etl_code.py, added datasources

--- etl_code.py
+++ etl_code.py
@@ -4,6 +4,27 @@ import xml.etree.ElementTree as ET
 from datetime import datetime 
 
 
+'''
+#? Complete the following practice exercises:
+
+#* 1- Create a folder data_source and use the terminal shell to change the current directory to 
+#*    \home\project\data_source. Create a file etl_practice.py in this folder.
+
+#* 2- Download and unzip the data available in the link shared above.
+
+#* 3- The data available has four headers: 'car_model', 'year_of_manufacture', 'price', 'fuel'. Implement the extraction 
+#*    process for the CSV, JSON, and XML files.
+
+#* 4- Transform the values under the 'price' header such that they are rounded to 2 decimal places.
+
+#* 5- Implement the loading function for the transformed data to a target file, transformed_data.csv.
+
+#* 6- Implement the logging function for the entire process and save it in log_file.txt.
+
+#* 7- Test the implemented functions and log the events as done in the lab.
+'''
+
+
 log_file = "log_file.txt" 
 target_file = "transformed_data.csv" 
 
@@ -19,7 +40,7 @@ def extract_from_json(file_to_process):
 
 
 def extract_from_xml(file_to_process):
-    dataframe = pd.DataFrame(colums=["name", "height", "weight"])
+    dataframe = pd.DataFrame(columns=["name", "height", "weight"])
     tree = ET.parse(file_to_process)
     root = tree.getroot()
     for person in root:
@@ -47,3 +68,57 @@ def extract():
         extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_xml(xmlfile))], ignore_index=True) 
          
     return extracted_data 
+
+
+def transform(data): 
+    '''Convert inches to meters and round off to two decimals 
+    1 inch is 0.0254 meters '''
+    data['height'] = round(data.height * 0.0254,2) 
+ 
+    '''Convert pounds to kilograms and round off to two decimals 
+    1 pound is 0.45359237 kilograms '''
+    data['weight'] = round(data.weight * 0.45359237,2) 
+    
+    return data 
+
+
+def load_data(target_file, transformed_data): 
+    transformed_data.to_csv(target_file) 
+    
+    
+def log_progress(message): 
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open(log_file,"a") as f: 
+        f.write(timestamp + ',' + message + '\n') 
+        
+        
+# Log the initialization of the ETL process 
+log_progress("ETL Job Started") 
+ 
+# Log the beginning of the Extraction process 
+log_progress("Extract phase Started") 
+extracted_data = extract() 
+ 
+# Log the completion of the Extraction process 
+log_progress("Extract phase Ended") 
+ 
+# Log the beginning of the Transformation process 
+log_progress("Transform phase Started") 
+transformed_data = transform(extracted_data) 
+print("Transformed Data") 
+print(transformed_data) 
+ 
+# Log the completion of the Transformation process 
+log_progress("Transform phase Ended") 
+ 
+# Log the beginning of the Loading process 
+log_progress("Load phase Started") 
+load_data(target_file,transformed_data) 
+ 
+# Log the completion of the Loading process 
+log_progress("Load phase Ended") 
+ 
+# Log the completion of the ETL process 
+log_progress("ETL Job Ended") 
